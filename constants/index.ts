7f6ab6e5d026c5e8efda66360adeffbb6e5b882d
jorge-ivan-jimenez-reyes <0251139@up.edu.mx>
2024-03-18 23:12:39 -0600

    skills

--- constants/index.ts
+++ constants/index.ts
@@ -202,12 +202,7 @@ export const Skill_data = [
   
   
   export const Other_skill = [
-    {
-      skill_name: "Go",
-      Image: "/go.png",
-      width: 60,
-      height: 60,
-    },
+
   ];
   
   
