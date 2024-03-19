5f1534edf8db79fb906051f6fab6d9b964a0d234
jorge-ivan-jimenez-reyes <0251139@up.edu.mx>
2024-03-18 23:37:01 -0600

    debug

--- components/main/Skills.tsx
+++ components/main/Skills.tsx
@@ -1,24 +1,25 @@
-import {
-  Backend_skill,
-  Frontend_skill,
-  Full_stack,
-  Skill_data,
-} from "@/constants";
+import { Backend_skill, Frontend_skill, Full_stack, Skill_data } from "@/constants";
 import React from "react";
 import SkillDataProvider from "../sub/SkillDataProvider";
 import SkillText from "../sub/SkillText";
+
+interface ImageData {
+  Image: string;
+  width: number;
+  height: number;
+}
 
 const Skills = () => {
   return (
     <section
       id="skills"
       className="flex flex-col items-center justify-center gap-3 h-full relative overflow-hidden pb-80 py-20"
-      style={{ transform: "scale(0.9" }}
+      style={{ transform: "scale(0.9)" }}
     >
       <SkillText />
 
       <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
-        {Skill_data.map((image, index) => (
+        {(Skill_data as ImageData[]).map((image, index) => (
           <SkillDataProvider
             key={index}
             src={image.Image}
@@ -30,7 +31,7 @@ const Skills = () => {
       </div>
 
       <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
-        {Frontend_skill.map((image, index) => (
+        {(Frontend_skill as ImageData[]).map((image, index) => (
           <SkillDataProvider
             key={index}
             src={image.Image}
@@ -41,7 +42,7 @@ const Skills = () => {
         ))}
       </div>
       <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
-        {Backend_skill.map((image, index) => (
+        {(Backend_skill as ImageData[]).map((image, index) => (
           <SkillDataProvider
             key={index}
             src={image.Image}
@@ -52,7 +53,7 @@ const Skills = () => {
         ))}
       </div>
       <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
-        {Full_stack.map((image, index) => (
+        {(Full_stack as ImageData[]).map((image, index) => (
           <SkillDataProvider
             key={index}
             src={image.Image}
@@ -62,8 +63,6 @@ const Skills = () => {
           />
         ))}
       </div>
-      
-
       <div className="w-full h-full absolute">
         <div className="w-full h-full z-[-10] opacity-30 absolute flex items-center justify-center bg-cover">
           <video
