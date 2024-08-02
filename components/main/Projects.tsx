c1131d2ab1e640288819ea71e879bd738007e1b1
jorge-ivan-jimenez-reyes <0251139@up.edu.mx>
2024-08-02 00:35:37 -0600

    added grid

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -10,39 +10,31 @@ const Projects = () => {
       <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 mb-16">
         My Projects
       </h1>
-      <div className="w-full flex flex-col md:flex-row flex-wrap gap-6 px-5 justify-center">
-        <div className="w-80 mb-10 md:mb-0">
-          <ProjectCard
-            src="/ioslab.png"
-            title="IOS LAB PORTFOLIO"
-            description="The platform provides an intuitive and comprehensive interface that allows
-            professors, department heads, and administrators to access and manage information efficiently."
-          />
-        </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-10 px-5 justify-center max-w-7xl mx-auto">
+        <ProjectCard
+          src="/ioslab.png"
+          title="IOS LAB PORTFOLIO"
+          description="The platform provides an intuitive and comprehensive interface that allows
+          professors, department heads, and administrators to access and manage information efficiently."
+        />
 
-        <div className="w-80 mb-10 md:mb-0">
-          <ProjectCard
-            src="/SpaceWebsite.png"
-            title="Space Themed Website"
-            description="Web portfolio for a space-themed website. The website is built using React js and Tailwind CSS."
-          />
-        </div>
+        <ProjectCard
+          src="/SpaceWebsite.png"
+          title="Space Themed Website"
+          description="Web portfolio for a space-themed website. The website is built using React js and Tailwind CSS."
+        />
 
-        <div className="w-80 mb-10 md:mb-0">
-          <ProjectCard
-            src="/dashuser.png"
-            title="Academic Management Dashboard"
-            description="In collaboration with the IOS Lab and the Faculty of Engineering at Universidad Panamericana, I am developing a user dashboard designed for academic management. This platform allows faculty members to efficiently manage their profiles and upload their CVs, significantly enhancing the organization and accessibility of academic information."
-          />
-        </div>
+        <ProjectCard
+          src="/dashuser.png"
+          title="Academic Management Dashboard"
+          description="In collaboration with the IOS Lab and the Faculty of Engineering at Universidad Panamericana, I am developing a user dashboard designed for academic management. This platform allows faculty members to efficiently manage their profiles and upload their CVs, significantly enhancing the organization and accessibility of academic information."
+        />
 
-        <div className="w-80 mb-10 md:mb-0">
-          <ProjectCard
-            src="/chatgptlocal.png"
-            title="Local ChatGPT with Llama 2"
-            description="In this cutting-edge project, I've built a local version of ChatGPT using the Llama 2 model. This implementation, crafted with vanilla JavaScript, allows users to experience advanced AI capabilities directly on their own machines, ensuring privacy and performance without relying on cloud-based services. Dive into the future of AI with a seamless, locally hosted solution."
-          />
-        </div>
+        <ProjectCard
+          src="/chatgptlocal.png"
+          title="Local ChatGPT with Llama 2"
+          description="In this cutting-edge project, I've built a local version of ChatGPT using the Llama 2 model. This implementation, crafted with vanilla JavaScript, allows users to experience advanced AI capabilities directly on their own machines, ensuring privacy and performance without relying on cloud-based services. Dive into the future of AI with a seamless, locally hosted solution."
+        />
       </div>
     </div>
   );
--- components/sub/ProjectCard.tsx
+++ components/sub/ProjectCard.tsx
@@ -9,18 +9,18 @@ interface Props {
 
 const ProjectCard = ({ src, title, description }: Props) => {
   return (
-    <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61] transform hover:scale-105 transition-transform duration-300 ease-in-out">
+    <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61] transform hover:scale-105 transition-transform duration-300 ease-in-out w-full h-[500px] flex flex-col">
       <Image
         src={src}
         alt={title}
         width={1000}
         height={1000}
-        className="w-full h-48 object-cover rounded-t-lg"
+        className="w-full h-64 object-cover"
       />
 
-      <div className="relative p-4 bg-gradient-to-r from-gray-900 via-purple-900 to-gray-900 rounded-b-lg">
-        <h1 className="text-2xl font-semibold text-white">{title}</h1>
-        <p className="mt-2 text-gray-300">{description}</p>
+      <div className="flex flex-col justify-between flex-grow p-6 bg-gradient-to-r from-gray-900 via-purple-900 to-gray-900 rounded-b-lg">
+        <h1 className="text-3xl font-semibold text-white">{title}</h1>
+        <p className="mt-4 text-lg text-gray-300">{description}</p>
       </div>
     </div>
   );
