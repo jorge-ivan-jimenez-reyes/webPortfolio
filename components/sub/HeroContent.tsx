dcc875988a1a7924787e131d4aa61b573a56cb17
jorge-ivan-jimenez-reyes <0251139@up.edu.mx>
2024-08-17 23:13:50 -0600

    added layput

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -1,14 +1,15 @@
-"use client";
-
 import React, { Suspense } from "react";
 import Spline from '@splinetool/react-spline/next';
 
 const HeroContent = () => {
   return (
     <div className="h-screen w-full flex flex-col items-center justify-center">
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<div>Loading Spline scene...</div>}>
         <div className="h-1/2 w-full md:w-3/4 lg:w-2/3">
-          <Spline scene="https://prod.spline.design/Nv5V8ifD3xBeWDkR/scene.splinecode" />
+          <Spline 
+            scene="https://prod.spline.design/Nv5V8ifD3xBeWDkR/scene.splinecode"
+            onError={(error) => console.error('Error loading Spline scene:', error)}
+          />
         </div>
       </Suspense>
       <div className="text-center mt-10">
