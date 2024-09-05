68a981f4629b19bdaa0947c10734fde429fcc60f
jorge-ivan-jimenez-reyes <0251139@up.edu.mx>
2024-09-05 10:12:09 -0600

    change stykes

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}
+        className={`${inter.className} bg-gradient-to-b from-[#030014] to-[#000000] overflow-y-scroll overflow-x-hidden`}
       >
         {children}
       </body>
